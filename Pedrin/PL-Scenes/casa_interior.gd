1b6135a04fed045b27021d40cf3c15fad8d96bde
Pedro Luiz <pedrin.luiz22@gmail.com>
2026-08-28 18:47:55 -0300

    spawn para entrada e saida de casa

--- Pedrin/PL-Scenes/casa_interior.gd
+++ Pedrin/PL-Scenes/casa_interior.gd
@@ -1,16 +1,10 @@
 extends Node2D
-
 @onready var player = $PlayerTd/Player
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	if Global.entrada_alvo == "":
-		var spawn_inicial = get_node_or_null("SpawnPoint")
-		if spawn_inicial:
-			player.global_position = spawn_inicial.global_position
-	else:
+	if Global.entrada_alvo != "":
 		var spawn_carregado = get_node_or_null(Global.entrada_alvo)
 		if spawn_carregado:
 			player.global_position = spawn_carregado.global_position
-		
 		Global.entrada_alvo = ""
--- Pedrin/PL-Scripts/entrar_casa.gd
+++ Pedrin/PL-Scripts/entrar_casa.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
-@export var cena_casa: PackedScene
+@export_file ("*.tscn") var cena_caminho: String
 @export var spawn_destino: String
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		if cena_casa:
+		if cena_caminho != "":
 			Global.entrada_alvo = spawn_destino
-			get_tree().change_scene_to_packed(cena_casa)
+			get_tree().change_scene_to_file(cena_caminho)
 		else:
 			print("Conecte no inspector a cena")
 	else:
