a3a30ca3afd45a43984bb3d574e5d7399db0b8fb
Pedro Luiz <pedrin.luiz22@gmail.com>
2026-08-28 19:56:10 -0300

    Sistema de diae e noite

---
+++ Pedrin/PL-Scripts/SistemaTempo.gd
@@ -0,0 +1,31 @@
+extends Node
+
+signal tempo_atualizado(hora: int, minuto: int)
+signal meia_noite
+
+const MINUTOS_TOTAIS_DIA = 17.0 * 60.0 
+const SEGUNDOS_REAIS = 15.0
+
+var tempo_passado: float = 0.0
+var relogio_rodando: bool = true
+var fracao_dia: float = 0.0
+
+func _process(delta: float) -> void:
+	if not relogio_rodando: return
+	
+	tempo_passado += delta
+	fracao_dia = clamp(tempo_passado / SEGUNDOS_REAIS, 0.0, 1.0)
+	
+	var minutos_in_game = int(fracao_dia * MINUTOS_TOTAIS_DIA)
+	var hora = 7 + (minutos_in_game / 60)
+	var minuto = minutos_in_game % 60
+	
+	tempo_atualizado.emit(hora, minuto)
+	
+	if hora >= 24:
+		relogio_rodando = false
+		meia_noite.emit()
+
+func resetar_dia() -> void:
+	tempo_passado = 0.0
+	relogio_rodando = true
--- Pedrin/PL-Scripts/fazenda.gd
+++ Pedrin/PL-Scripts/fazenda.gd
@@ -1,16 +1,30 @@
 extends Node2D
 
 @onready var player = $PlayerTd/Player
+@onready var tela_escura = $CanvasLayer/ColorRect 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+
 	if Global.entrada_alvo == "":
 		var spawn_inicial = get_node_or_null("SpawnPoint")
-		if spawn_inicial:
-			player.global_position = spawn_inicial.global_position
+		if spawn_inicial: player.global_position = spawn_inicial.global_position
 	else:
 		var spawn_carregado = get_node_or_null(Global.entrada_alvo)
-		if spawn_carregado:
-			player.global_position = spawn_carregado.global_position
-		
+		if spawn_carregado: player.global_position = spawn_carregado.global_position
 		Global.entrada_alvo = ""
+
+	# Conecta o sinal da meia-noite
+	SistemaTempo.meia_noite.connect(_desmaiar_meia_noite)
+
+func _desmaiar_meia_noite() -> void:
+	var tween = create_tween()
+	# Faz o ColorRect ir do Alpha 0 (transparente) para 1 (preto sólido) em 2 segundos
+	tween.tween_property(tela_escura, "modulate:a", 1.0, 2.0)
+	
+	# Quando o fade terminar, chama a função de trocar de cena
+	tween.tween_callback(_acordar_na_casa)
+
+func _acordar_na_casa() -> void:
+	Global.entrada_alvo = "SpawnCama" # Crie um Marker2D na casa perto da cama com esse nome
+	SistemaTempo.resetar_dia() # Reinicia o relógio para as 7h
+	get_tree().change_scene_to_file("res://casa_interior.tscn") # Ajuste o nome do arquivo
