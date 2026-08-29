e951fcb4fe8207ebdee95de4d35740ffd225549c
kauasouzaffernandes <kaua.souza@alunos.ifsuldeminas.edu.br>
2026-08-29 07:14:37 -0300

    falas do velho sabido

--- Pedrin/PL-Scripts/velhosabio.gd
+++ Pedrin/PL-Scripts/velhosabio.gd
@@ -6,18 +6,18 @@ extends Interactable
 var dialogo_aberto: bool = false
 
 var dicas_diarias = {
-	1: "Olá, jovem! O Milho mal precisa de água na Primavera, mas o Tomate já exige mais cuidado.",
-	2: "Sementes não plantadas no primeiro dia apodrecem. Espero que tenha usado as suas!",
-	3: "Amanhã o Verão começa! Prepare o regador, pois o sol castiga as plantas.",
-	4: "Chegou o Verão! Os tomates vão precisar de 3 regadas diárias e 2 porções de fertilizante.",
-	5: "Dica de ouro: regue e adube logo de manhã para não esquecer.",
-	6: "O Outono se aproxima. O clima vai esfriar um pouco e a sede das plantas vai diminuir.",
-	7: "Folhas caindo... No Outono, o Tomate nem precisa de fertilizante, apenas água.",
-	8: "Fique de olho na cor da terra para saber se você já a regou hoje.",
-	9: "O Inverno chega amanhã! O frio extremo exige que você adube muito a terra para mantê-la quente.",
-	10: "No Inverno o Milho não precisa de água, mas consome muito fertilizante!",
-	11: "Falta pouco para o fim do ano! Mantenha o foco nos cuidados diários.",
-	12: "Último dia! Faça sua ronda final. Amanhã avaliaremos o sucesso da sua colheita."
+	1: "Tomatuxos no verão gostam de muita água.",
+	2: "Tomatuxos precisam de menos adubo no verão.",
+	3: "Sojentas no verão bebem pouca água.",
+	4: "sojentas no frio crescem mais e bebem mais água.",
+	5: "Os Tomatuxos no outono gostam de adubo.",
+	6: "Quanto mais frio menos os Tomatuxos bebm água.",
+	7: "As Sojentas no frio devem ser regadas dia sim dia não.",
+	8: "As sojentas não bebem água ao invés disso precisam de adubo se não morrem.",
+	9: "Para os Tomatuxos o equilíbrio é a chave.",
+	10: "1 de cadajá basta.",
+	11: "os Tomatuxos na primavera gostam de bastante água.",
+	12: "3 Sojentas são letais."
 }
 
 func _ready() -> void:
