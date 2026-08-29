34af5d4ee78acb29191aca34b41029e1466a5a14
Pedro Luiz <pedrin.luiz22@gmail.com>
2026-08-29 11:26:19 -0300

    fix

--- Pedrin/PL-Scripts/casa_interior.gd
+++ Pedrin/PL-Scripts/casa_interior.gd
@@ -1,11 +1,18 @@
 extends Node2D
-@onready var player = $PlayerTd/Player
 
+@onready var player = $PlayerTd
 
 func _ready() -> void:
-	# Faz a casa escutar o relógio e te fazer dormir se der meia noite
+	if Global.entrada_alvo != "":
+		# find_child procura em todos os níveis e pastas da cena
+		var spawn_carregado = find_child(Global.entrada_alvo, true, false)
+		
+		if spawn_carregado: 
+			player.global_position = spawn_carregado.global_position
+		
+		Global.entrada_alvo = ""
+
 	SistemaTempo.meia_noite.connect(_desmaiar_na_casa)
 
 func _desmaiar_na_casa() -> void:
-	# Passa o dia automaticamente e verifica as regras
 	Global.dormir_e_processar_dia(get_tree())
--- Pedrin/PL-Scripts/entrar_casa.gd
+++ Pedrin/PL-Scripts/entrar_casa.gd
@@ -3,12 +3,15 @@ extends Area2D
 @export_file ("*.tscn") var cena_caminho: String
 @export var spawn_destino: String
 
-
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
+	if body.name == "PlayerTd":
 		if cena_caminho != "":
 			Global.entrada_alvo = spawn_destino
-			get_tree().change_scene_to_file(cena_caminho)
+			
+			# O Godot agora vai agendar a troca de cena para a fração de segundo 
+			# seguinte, após terminar de calcular a colisão com segurança.
+			get_tree().call_deferred("change_scene_to_file", cena_caminho)
+			
 		else:
 			print("Conecte no inspector a cena")
 	else:
